7cb142c7178f004be03d8d1c92681dd5f936f89a
Hassam-Ata <hassamata86@gmail.com>
2024-11-01 02:23:28 +0500

    fixed navbar

--- Frontend/src/components/AboutHeader.jsx
+++ Frontend/src/components/AboutHeader.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import styles from "../styles/AboutHeader.module.css"
+import React from "react";
+import styles from "../styles/AboutHeader.module.css";
 
 const AboutHeader = ({ heading, text, imageSrc }) => {
   return (
     <div className={styles.container}>
-      {/* <img src={imageSrc} alt="About Us" className={styles.image} /> */}
       <div className={styles.textBlock}>
         <h1 className={styles.heading}>{heading}</h1>
         <p className={styles.text}>{text}</p>
       </div>
     </div>
   );
-}
+};
 
-export default AboutHeader
+export default AboutHeader;
